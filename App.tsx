0227fb5a6fca57c2a0c0634f13f804d1dfd66292
GChimel <gustavochimel12@gmail.com>
2024-08-07 12:57:33 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { config } from './config/gluestack-ui.config';
+import { Loading } from '@components/loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  return (
+    <GluestackUIProvider config={config}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent />
+      {fontsLoaded ? (
+        <Center flex={1} bg="$gray700">
+          <Text>Hello World</Text>
+        </Center>
+      ) : (
+        <Loading />
+      )}
+    </GluestackUIProvider>
+  );
+}
