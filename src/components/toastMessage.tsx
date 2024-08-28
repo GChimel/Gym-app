72d3e359987cd9637bc940c2d77336be6d9692df
GChimel <gustavochimel12@gmail.com>
2024-08-28 06:01:21 -0300

    User select photo

---
+++ src/components/toastMessage.tsx
@@ -0,0 +1,50 @@
+import {
+  Pressable,
+  Icon,
+  ToastDescription,
+  ToastTitle,
+  Toast,
+  VStack,
+} from "@gluestack-ui/themed";
+import { X } from "lucide-react-native";
+
+type Props = {
+  id: string;
+  title: string;
+  description?: string;
+  action?: "error" | "success";
+  onClose: () => void;
+};
+
+export function ToastMessage({
+  id,
+  title,
+  description,
+  action,
+  onClose,
+}: Props) {
+  return (
+    <Toast
+      nativeID={`toast-${id}`}
+      mt="$10"
+      action={action}
+      bg={action === "success" ? "$green500" : "$red500"}
+    >
+      <VStack space="xs" w="$full">
+        <Pressable alignSelf="flex-end" onPress={onClose}>
+          <Icon as={X} color="$coolGray50" size="md" />
+        </Pressable>
+
+        <ToastTitle color="$white" fontFamily="$heading">
+          {title}
+        </ToastTitle>
+
+        {description && (
+          <ToastDescription color="$white" fontFamily="$body">
+            {description}
+          </ToastDescription>
+        )}
+      </VStack>
+    </Toast>
+  );
+}
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,22 +1,70 @@
+import { useState } from "react";
 import { Button } from "@components/button";
 import { Input } from "@components/input";
 import { ScreenHeader } from "@components/screenHeader";
 import { UserPhoto } from "@components/userPhoto";
-import { Center, VStack, Text, Heading } from "@gluestack-ui/themed";
+import { Center, VStack, Text, Heading, useToast } from "@gluestack-ui/themed";
 import { ScrollView, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { ToastMessage } from "@components/toastMessage";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState("https://github.com/gchimel.png");
+  const toast = useToast();
+
+  // Seleção e edição da imagem
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        selectionLimit: 1,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+        // base64: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const photoURI = photoSelected.assets[0].uri;
+
+      if (photoURI) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            placement: "top",
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Imagem muito grande"
+                description="Escolha uma imagem de ate 5MB"
+                onClose={() => toast.close(id)}
+              />
+            ),
+          });
+        }
+        setUserPhoto(photoURI);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
-          <UserPhoto
-            source={{ uri: "https://github.com/gchimel.png" }}
-            alt="User photo"
-            size="xl"
-          />
-          <TouchableOpacity>
+          <UserPhoto source={{ uri: userPhoto }} alt="User photo" size="xl" />
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
